f0bbd0912e3cb8347389fbee96657ac28c753688
Sucheta Mahata <suchetamahata>
2021-09-25 12:38:19 +0530

    worked on osama's comments

--- src/pages.tsx/Entriespage.tsx
+++ src/pages.tsx/Entriespage.tsx
@@ -4,6 +4,7 @@ const Entriespage = () =>{
 
     return(
         <div className = 'container2'>
+            
             <div className = 'leftpanel'>
                 <input className='date' placeholder='10/9/2021'/>
                 <button className='buttonS' ></button>
