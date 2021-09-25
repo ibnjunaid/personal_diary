f3351193946b0727055be17eb957cb685be0f916
osama bin junaid <osama.junaid@c3m.io>
2021-09-25 12:47:43 +0530

    removed stray line

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import './App.scss';
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="container1">
-      <div className="title_div"><b>
-        <h1>My Diary</h1>
-        <p>- anonymous</p> {/* //TODO: display uname here with help of state */}
-        </b>
-      </div>
-      <div className="button_div1">
-        <button  className="signin">Sign in with google</button><br/>
-      </div>
-      <div className="button_div2">
-        <button className="login">Don't have an accout? <b>Sign Up</b></button>
-      </div>
-      <div className="about">
-        <button className="button3"> i </button></div>
-    </div>
+    <Home/>
   );
 }
 
