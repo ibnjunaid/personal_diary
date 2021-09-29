7f118af4e01cf85258843786562df63c46233fb7
Sucheta Mahata <suchetamahata>
2021-09-29 22:08:18 +0530

    entry card fetch

--- client/src/pages/Entries/Entriespage.tsx
+++ client/src/pages/Entries/Entriespage.tsx
@@ -1,19 +1,23 @@
 import { SearchOutlined } from '@ant-design/icons'
 import { useEffect, useState } from 'react';
 //import { Link } from 'react-router-dom';
-
+import {EntrySchema} from '../../../../commons/interfaces/entrySchema';
 
 import CreateEntry from '../../components/display/CreateEntry';
 import EntryCard from '../../components/display/EntryCard';
 import UserInfo from '../../components/display/UserInfo';
 import './Entriespage.scss';
 
+interface EntrySchemaResponse extends EntrySchema{
+   _id : string,
+   _v : string
+}
 
 const Entriespage = () => {
     const [title, setTitle] = useState<string>('Title')
     const [text, setText] = useState<string>('lorum ipsum dolor sit amet constructor a')
     const [bcolor, setBColor] = useState<string>('color1')
-    const [data, setTData] = useState<any>([])
+    const [data, setTData] = useState<Array<EntrySchemaResponse>>([])
 
     
     useEffect(() => {
@@ -26,8 +30,8 @@ const Entriespage = () => {
                         'Content-Type':"application/json"
                     }
                 });
-               let data = await response.json()
-                console.log(data);
+               let {data} = await response.json()
+                setTData(data)
             } catch (error) {
                 throw error;
             }
@@ -46,13 +50,13 @@ const Entriespage = () => {
                 <button className='createbtn'> Create New Entry + </button>
                 <button className='search'><SearchOutlined /></button>
                 <div className='entrydiv' >
-                    {/* {
+                    {
                         data.map((data : any) => {
                             return (
-                                <button className='entrycbtn'><EntryCard title={title} text={text} /></button>
+                                <button className='entrycbtn' key={data._id}><EntryCard title={title} text={text} /></button>
                             )
                         })
-                    } */}
+                    }
                     <button onClick={(e)=>{}} className='entrycbtn'><EntryCard title={title} text={text} /></button>
                 </div>
             </div>
